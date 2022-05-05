289e8884a9d8dcb348a7c8d0e7ce1c2913083f1e
BokiThe <ristic1989@gmail.com>
2022-05-05 17:58:22 +0200

    hero page added useEffect

--- src/screens/HeroPage/hero.js
+++ src/screens/HeroPage/hero.js
@@ -1,20 +1,22 @@
 import "./hero.css";
+import { react, useEffect } from "react";
 
 const HeroPage = () => {
   const fadeOut = () => {
     const dis = document.getElementById("disapear");
     dis.style.opacity = "0";
-    dis.style.transition = "all 2s";
+    dis.style.transition = "ease 1s";
     window.setTimeout(function removethis() {
       dis.style.display = "none";
     }, 2000);
   };
+  useEffect(() => {}, []);
   return (
     <div className="heroContainer" id="disapear">
       <div className="heroSection">
         <h1 className="heroText">HELLO!</h1>
         <h1 className="heroText">WELCOME TO MY PORTFOLIO PAGE</h1>
-        <button className="btnExplore " onClick={fadeOut}>
+        <button className="btnExplore heroBtn" onClick={fadeOut}>
           HOP UP!
         </button>
       </div>
